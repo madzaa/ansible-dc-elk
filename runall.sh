9bac22d6b4e25f6e68b85db8be886f9b9af9404c
Madis Salumäe <madis.salumae@gmail.com>
2018-09-20 13:24:21 +0100

    minor

--- runall.sh
+++ runall.sh
@@ -14,9 +14,6 @@ echo "##########################"
 sleep 25s
 # Make stuff
 mkdir -p /home/${USER}/logstash/; touch /home/${USER}/logstash/logfile.log
-# Ere me now
-sudo nc localhost 5041 < /home/${USER}/logstash/logfile.log
-# make sum logs
 # make it rain
 curl -XPOST -D- 'http://localhost:5601/api/saved_objects/index-pattern' \
     -H 'Content-Type: application/json' \
@@ -26,4 +23,5 @@ sleep 10s
 sudo bash -c " echo A bug is never just a mistake. It represents something bigger. A \
 n error of thinking. That makes you who you are. > /home/${USER}/logstash/logfile.log"
 sleep 10s
+nc localhost 5041 < /home/${USER}/logstash/logfile.log
 echo "Magic happens here - https://localhost:5601"
