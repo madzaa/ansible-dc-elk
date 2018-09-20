62969bbe59d3dcc85a9234a2e173aec1c255f339
Madis Salumäe <madis.salumae@gmail.com>
2018-09-20 11:21:03 +0100

    last last changes

--- runall.sh
+++ runall.sh
@@ -8,14 +8,21 @@ sudo apt install ansible
 sudo ansible-playbook tasks/main.yml
 # Start the ELK stack with docker compose
 sudo docker-compose -f dc-elk/docker-compose.yml up -d
+# ZZZ
+echo "Setting up ELK environment"
+echo "##########################"
+sleep 25s
 # Make stuff
-sudo mkdir -p /tmp/logstash/; sudo touch /tmp/logstash/logfile.log 
+sudo mkdir -p /home/${USER}/logstash/; sudo touch /home/${USER}/logstash/logfile.log
 # Ere me now
-sudo nc localhost 5041 < /tmp/logstash/logfile.log
+sudo nc localhost 5041 < /home/${USER}/logstash/logfile.log
 # make sum logs
-ehco "A bug is never just a mistake. It represents something bigger. An error of thinking. That makes you who you are." > /tmp/logstash/logfile.log
+sudo echo "A bug is never just a mistake. It represents something bigger. An error of thinking. That makes you who you are." > /home/${USER}/logstash/logfile.log
 # make it rain
 curl -XPOST -D- 'http://localhost:5601/api/saved_objects/index-pattern' \
     -H 'Content-Type: application/json' \
     -H 'kbn-version: 6.4.0' \
     -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
+echo "Magic inbound in 25s"
+sleep 25s
+echo "Open up https://localhost:5601 to see the magic"
