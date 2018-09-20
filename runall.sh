4ed969553198d2c937f04e80c92fff0c9a836cd4
Madis Salumäe <madis.salumae@gmail.com>
2018-09-20 10:45:32 +0100

    hello friend

--- runall.sh
+++ runall.sh
@@ -7,4 +7,13 @@ sudo apt install ansible
 # in the light of this project
 sudo ansible-playbook tasks/main.yml
 # Start the ELK stack with docker compose
-sudo docker-compose -f dc-elk/docker-compose.yml up 
+sudo docker-compose -f dc-elk/docker-compose.yml up -d
+# listen up
+sudo mkdir logs /tmp/logstash/logfile.log; sudo nc localhost 5041 < /tmp/logstash/logfile.log
+# make sum logs
+ehco "A bug is never just a mistake. It represents something bigger. An error of thinking. That makes you who you are." > /tmp/logstash/logfile.log
+# make it rain
+curl -XPOST -D- 'http://localhost:5601/api/saved_objects/index-pattern' \
+    -H 'Content-Type: application/json' \
+    -H 'kbn-version: 6.4.0' \
+    -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
