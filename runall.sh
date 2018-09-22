2b1102634db5436322cc85a66a38c425b086be78
Madis Salumäe <madis.salumae@gmail.com>
2018-09-22 20:24:50 +0100

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -2,7 +2,11 @@
 # The vm.max_map_count kernel setting needs to be set to at least 262144 for production use.
 sudo sysctl -w vm.max_map_count=262144
 # Install Ansible
-sudo apt install ansible
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
 # Run Ansible-playbook to download docker and docker-compose. NB! The defaults are already filled in
 # in the light of this project
 sudo ansible-playbook tasks/main.yml
